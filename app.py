17be8623faeb92a7435964cd4535623a30efda11
PieZu <39189047+PieZu@users.noreply.github.com>
2021-05-04 14:25:14 +1200

    ^u^ init

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("home.html")
+ 
+@app.route("/sensors")
+def about():
+    return render_template("sensors.html")
+
+
+app.run(debug=True)
