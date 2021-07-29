bb316e794f5683eb6d008056d81581cf4fe6eda6
PieZu <39189047+PieZu@users.noreply.github.com>
2021-07-29 13:12:04 +1200

    add export csv functionality/button

--- api/logs.py
+++ api/logs.py
@@ -4,7 +4,7 @@ import json
 from database.config import DATABASE_PATH
 import sqlite3
 from time import time
-from api.sensors import get_sensor_update_freq
+from api.sensors import get_sensor_update_freq, get_all_sensors
 
 def get_logs_from_sensor(sensor_id):
     with sqlite3.connect(DATABASE_PATH) as con:
@@ -34,11 +34,50 @@ def insert_logs(user_id, sensor_id, logs):
         con.commit()
     return True
 
+def dump_logs():
+    sensors = [name for _,name,_,_,_ in get_all_sensors()]
+    table = []
+    row_length = range(len(sensors)+1)
+
+    with sqlite3.connect(DATABASE_PATH) as con:
+        cur = con.cursor()
+        cur.execute("SELECT datetime(timestamp, 'unixepoch', 'localtime'), sensor, value FROM log ORDER BY timestamp ASC")
+
+        last_timestamp = None
+        last_row = []
+        for (timestamp, sensor, value) in cur:
+            # print(timestamp, sensor, value)
+            if not timestamp == last_timestamp:
+                table.append(last_row)
+                last_row = list(row_length)
+                last_row[0] = timestamp
+                last_timestamp = timestamp
+            last_row[sensor] = value
+        table.append(last_row)
+    table[0] = ["Time"]+sensors
+    return table
+
 api = Blueprint(
     'logapi', __name__,
     template_folder = 'templates',
     url_prefix = '/api/logs'
 )
+
+
+COMMA = ','
+QUOTE = '"'
+TWO_QUOTES = '""'
+@api.route('/dump')
+@authenticate("VIEW_LOGS")
+def get_csv():
+    table = dump_logs()
+    # first row is sensor names wrapped in "", with " marks escaped with ""
+    csv = COMMA.join(QUOTE+ name.replace(QUOTE, TWO_QUOTES) +QUOTE  for name in table[0])
+    for row in table[1:]:
+        csv += "\n" + COMMA.join(str(value) for value in row)
+    response = Response(csv, status=200, mimetype='text/csv')
+    response.headers['Content-Disposition'] = 'inline; filename="log_data.csv"'
+    return response
 
 @api.route('/<sensor_id>/last/', methods=["GET"])
 @authenticate("VIEW_LOGS")
